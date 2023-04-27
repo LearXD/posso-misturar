100c0674769288b9deb075ed7187b061994315cc
LearXD <gutyxdcanal@gmail.com>
2023-04-27 17:05:06 -0300

    Atualizando o Mixtures Manager

--- src/data/MixturesManager.js
+++ src/data/MixturesManager.js
@@ -1,5 +1,22 @@
 const data = require('./data.json');
 
+export const getFirstSelectItens = () => {
+  const newData = {
+    ...data
+  }
+  return newData.map(product => product.name)
+}
+
+export const getSecondSelectItens = (firstSelectValue) => {
+  const product = data.find(product => product.name === firstSelectValue)
+  if (!product) {
+    throw new Error(`Product ${firstSelectValue} not found`)
+  }
+
+  const { combinations } = product;
+  return combinations.map(combination => combination.name)
+}
+
 export const combineWith = (a, b) => {
   const productA = data.find(product => product.name === a)
   if (!productA) {
